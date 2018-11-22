79d09440e92fa7f0b8ac59f545d387e8ff7f5720
95.nagy.d@gmail.com <Rbableves9>
2018-11-22 16:35:10 +0100

    made listing code simpler

--- student/js/listCars.js
+++ student/js/listCars.js
@@ -3,9 +3,7 @@ function listCars() {
         url: '/cars',
         type: 'get',
         success: function (carData) {
-            let resultTable = $(document).find('#carListTable');
-            let tableColumnNames = getCarColumnNames();
-            resultTable.append(tableColumnNames);
+            let resultTable = $(document).find('#carTable');
             $.each(carData, function (idx, value) {
                 addRowToTable(value, resultTable);
             });
@@ -14,23 +12,4 @@ function listCars() {
             window.alert('Could not load cars.');
         }
     });
-}
-
-function getCarColumnNames() {
-    let tableColumnNames = $('<tr class="resultTableColumnNames"></tr>');
-    let nameCell = $('<td class="resultTableColumnName">'+"NAME"+'</td>');
-    let consumptionCell = $('<td class="resultTableColumnName">'+"CONSUMPTION"+'</td>');
-    let colorCell = $('<td class="resultTableColumnName">'+"COLOR"+'</td>');
-    let manufacturerCell = $('<td class="resultTableColumnName">'+"MANUFACTURER"+'</td>');
-    let availableCell = $('<td class="resultTableColumnName">'+"AVAILABLE"+'</td>');
-    let yearCell = $('<td class="resultTableColumnName">'+"YEAR"+'</td>');
-    let horse_powerCell = $('<td class="resultTableColumnName">'+"HORSEPOWER"+'</td>');
-    tableColumnNames.append(nameCell);
-    tableColumnNames.append(consumptionCell);
-    tableColumnNames.append(colorCell);
-    tableColumnNames.append(manufacturerCell);
-    tableColumnNames.append(availableCell);
-    tableColumnNames.append(yearCell);
-    tableColumnNames.append(horse_powerCell);
-    return tableColumnNames;
 }
--- student/js/listManufacturers.js
+++ student/js/listManufacturers.js
@@ -3,9 +3,7 @@ function listManufacturers() {
         url: '/manufacturers',
         type: 'get',
         success: function (manufacturerData) {
-            let resultTable = $(document).find('#manufacturerListTable');
-            let tableColumnNames = getManufacturerColumnNames();
-            resultTable.append(tableColumnNames);
+            let resultTable = $(document).find('#manufacturerTable');
             $.each(manufacturerData, function (idx, value) {
                 addRowToTable(value, resultTable);
             });
@@ -14,15 +12,4 @@ function listManufacturers() {
             window.alert('Could not load any manufacturer.');
         }
     });
-}
-
-function getManufacturerColumnNames() {
-    let tableColumnNames = $('<tr class="resultTableColumnNames"></tr>');
-    let nameCell = $('<td class="resultTableColumnName">'+"NAME"+'</td>');
-    let countryCell = $('<td class="resultTableColumnName">'+"COUNTRY"+'</td>');
-    let foundedCell = $('<td class="resultTableColumnName">'+"FOUNDED"+'</td>');
-    tableColumnNames.append(nameCell);
-    tableColumnNames.append(countryCell);
-    tableColumnNames.append(foundedCell);
-    return tableColumnNames;
 }
