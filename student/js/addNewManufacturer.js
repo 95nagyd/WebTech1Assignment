9b09e39dc852bef6fac9bd9dfcec57e48b8d058a
nagy99 <95.nagy.d@gmail.com>
2018-11-23 09:16:36 +0100

    date fromat

--- student/js/addNewManufacturer.js
+++ student/js/addNewManufacturer.js
@@ -18,8 +18,34 @@ function createManufacturerObject() {
     manufacturer.name = formManufacturer.name.value;
     manufacturer.country = formManufacturer.country.value;
     let date = new Date(formManufacturer.founded.value);
-    const opts = {year: 'numeric', month: 'numeric', day: 'numeric'};
-    manufacturer.founded = date.toLocaleDateString("en-US", opts);
+    let month;
+    if(date.getMonth()+1===1){
+        month = "January";
+    }else if(date.getMonth()+1===2){
+        month = "February";
+    }else if(date.getMonth()+1===3){
+        month = "March";
+    }else if(date.getMonth()+1===4){
+        month = "April";
+    }else if(date.getMonth()+1===5){
+        month = "May";
+    }else if(date.getMonth()+1===6){
+        month = "June";
+    }else if(date.getMonth()+1===7){
+        month = "July";
+    }else if(date.getMonth()+1===8){
+        month = "August";
+    }else if(date.getMonth()+1===9){
+        month = "September";
+    }else if(date.getMonth()+1===10){
+        month = "October";
+    }else if(date.getMonth()+1===11){
+        month = "November";
+    }else{
+        month = "December";
+    }
+    date = month+" "+date.getDate()+", "+date.getFullYear();
+    manufacturer.founded = date;
     return manufacturer;
 }
 
